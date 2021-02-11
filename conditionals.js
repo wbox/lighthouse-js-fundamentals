fe86ee488900eef7365e0a66797282de1e8a4db8
Samuel Ribas <samuel.ribas@shopify.com>
2021-02-10 21:44:23 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,6 @@
 const raining = true;
 const cold = false;
-const temperature = 12;
+const temperature = 45;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
@@ -20,5 +20,19 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
