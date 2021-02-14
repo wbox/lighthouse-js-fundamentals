ea1ce8db13fbba1e10aca54e4e2dbdf596b96454
wbox <sribas@gmail.com>
2021-02-14 00:49:29 +0000

    commiting hello.js

---
+++ loop.js
@@ -0,0 +1,3 @@
+for (let number = 100; number <= 200; number++) {
+  (number % 3 === 0 && number % 4 ===0) ? console.log("LoopyLighthouse") : number % 3 === 0 ? console.log("Loopy") : number % 4 === 0 ? console.log("Lighthouse") : console.log(number);
+}
---
+++ udacity/tests.js
@@ -0,0 +1,14 @@
+/*
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
+}
+*/
+
+isThisWorking(3);
+
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+}
+
+isThisWorking(3);
